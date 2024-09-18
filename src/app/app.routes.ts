13ad57f963f82967fe52d8fe8d2b77dc65922caf
printetqt <villaesterprincess2016@gmail.com>
2024-09-18 22:44:24 +0800

    added nav bar and nav links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NgModule } from '@angular/core';
+export const routes: Routes = [
+ {
+    path: 'landing-page', component: LandingPageComponent
+ },{
+    path: 'home', component: HomePageComponent
+    
+    },{
+    path: 'login', component: LoginComponent
+    },{
+    path: 'signup', component: SignUpComponent
+    }
+    
+ 
 
-export const routes: Routes = [];
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule {}
